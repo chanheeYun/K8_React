d884e73d4acb6f7381313bc41c38f340c4f53ef0
chanheeYun <ych1782@naver.com>
2024-09-30 09:30:08 +0900

    Clock

--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -8,7 +8,7 @@ function MyClockTime() {
          setCTime(new Date());
       }, 1000); //한번 부르면 컴포넌트 없어질 때까지 주구장창 계속돈다 그래서 return 시켜줘야 해
 
-      return clearInterval(tm)
+      return() => {clearInterval(tm)}
    }, []);
    return (
       <p className='text-4xl text-blue-950 time font-bold'>

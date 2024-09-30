6674cd1aca64dddf2e699a6ac4f3c273c3e5486d
chanheeYun <ych1782@naver.com>
2024-09-30 09:42:58 +0900

    Clock

--- src/02/MyClockImg.js
+++ src/02/MyClockImg.js
@@ -3,7 +3,7 @@ import clock from '../img/clock2.png'
 function MyClockImg() {
    return(
       <div className='w-full flex justify-center items-center'>
-         <img src={clock} className='w-60 h-60 mt-12 mb-9 clock' alt='clock' />
+         <img src={clock} className='w-80 h-80 mt-12 mb-9 clock' alt='clock' />
       </div>
    );
 }
